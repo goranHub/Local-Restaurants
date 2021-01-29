7845478c771cd42bb4320ea1a0245d4e4b442f87
gsico <goran22sico@gmail.com>
2021-01-29 12:46:13 +0100

    add CustomInfoWindowGoogleMap

--- app/src/main/java/com/sicoapp/localrestaurants/ui/map/MapFragment.kt
+++ app/src/main/java/com/sicoapp/localrestaurants/ui/map/MapFragment.kt
@@ -1,35 +1,30 @@
 package com.sicoapp.localrestaurants.ui.map
 
-import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.sicoapp.localrestaurants.R
 import com.sicoapp.localrestaurants.data.remote.response.RestaurantResponse
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.dialog_progress.*
 import kotlinx.android.synthetic.main.fragment_map.*
-import timber.log.Timber
 
 
 @AndroidEntryPoint
-class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback,
-    GoogleMap.OnMarkerClickListener {
+class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback{
 
     private val viewModel: MapViewModel by viewModels()
     lateinit var mMapFragment: SupportMapFragment
+    lateinit var googleMap: GoogleMap
 
 
     override fun onCreateView(
@@ -54,55 +49,29 @@ class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback,
         viewModel.showMapCallback = object : ShowMapCallback {
 
             override fun onResponse(it: List<RestaurantResponse>) {
-                it.map {
+                it.map { response ->
 
-                    val position = LatLng(it.latitude, it.longitude)
-                    val name = it.name
-                    googleMap.addMarker(MarkerOptions().position(position).title(name))
+                    val position = LatLng(response.latitude, response.longitude)
+                    val name = response.name
+
+                    val info  = response
+
+                    val markerOptions = MarkerOptions()
+                    markerOptions.position(position)
+                        .title(name)
+                        .snippet("I am custom Location Marker.")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+
                     val newLatLngZoom = CameraUpdateFactory.newLatLngZoom(position, 13f)
                     googleMap.animateCamera(newLatLngZoom)
+                    googleMap.setInfoWindowAdapter(CustomInfoWindowGoogleMap(this@MapFragment ))
 
+                    val marker = googleMap.addMarker(markerOptions)
+                    marker.tag = info
+                    marker.showInfoWindow()
                 }
             }
         }
-
-        googleMap.setOnMarkerClickListener(this@MapFragment)
-    }
-
-    override fun onMarkerClick(marker: Marker?): Boolean {
-        val locAddress = marker!!.title
-        alertDialog(locAddress)
-        Timber.d(locAddress)
-        return true
-    }
-
-    private fun alertDialog(name: String) {
-        val builder = AlertDialog.Builder(requireContext())
-        builder.setTitle("Restaurant")
-        builder.setMessage(name)
-        //builder.setPositiveButton("OK", DialogInterface.OnClickListener(function = x))
-
-        builder.setPositiveButton(android.R.string.yes) { dialog, which ->
-            Toast.makeText(
-                requireContext(),
-                android.R.string.yes, Toast.LENGTH_SHORT
-            ).show()
-        }
-
-        builder.setNegativeButton(android.R.string.no) { dialog, which ->
-            Toast.makeText(
-                requireContext(),
-                android.R.string.no, Toast.LENGTH_SHORT
-            ).show()
-        }
-
-        builder.setNeutralButton("Maybe") { dialog, which ->
-            Toast.makeText(
-                requireContext(),
-                "Maybe", Toast.LENGTH_SHORT
-            ).show()
-        }
-        builder.show()
     }
 
     override fun onResume() {
